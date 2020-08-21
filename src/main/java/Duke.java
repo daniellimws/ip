7e4ac0986b71ca8f18c6bc88c01ae067a91f707a
Daniel Lim Wee Soong <weesoong.lim@gmail.com>
2020-08-21 18:00:49 +0800

    Improve greeting and respond to user input
    - Currently only echos the user input, or exits the program if user says
    "bye"
    - Change duke logo to robot head

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,43 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("What can I do for you?");
-        
-        System.out.println("Bye. Hope to see you again soon!");
+        String logo = "" +
+            "                 _______\n"+
+            "               _/       \\_\n"+
+            "              / |       | \\\n"+
+            "             /  |__   __|  \\\n"+
+            "            |__/((o| |o))\\__|\n"+
+            "            |      | |      |\n"+
+            "            |\\     |_|     /|\n"+
+            "            | \\           / |\n"+
+            "             \\| /  ___  \\ |/\n"+
+            "              \\ | / _ \\ | /\n"+
+            "               \\_________/\n"+
+            "                _|_____|_\n"+
+            "           ____|_________|____\n"+
+            "          /                   \\\n";
+        System.out.println(logo);
+        System.out.println("Hello. What can I do for you?");
+
+        Scanner in = new Scanner(System.in);
+        Boolean isRunning = true;
+
+        while (isRunning) {
+            String cmd = in.nextLine();
+
+            System.out.print("  ");
+            switch (cmd.toLowerCase()) {
+            case "bye":
+                System.out.println("Bye. Hope to see you again soon!");
+                isRunning = false;
+                break;
+            default:
+                System.out.println(cmd);
+                break;
+            }
+        }
+
+        in.close();
     }
 }
