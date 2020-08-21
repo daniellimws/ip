6541e30825bd2744f75ff5c7f7f83f0c7720f423
Daniel Lim Wee Soong <weesoong.lim@gmail.com>
2020-08-21 19:22:11 +0800

    Implement storing of user input in an array

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,33 @@
 import java.util.Scanner;
 
 public class Duke {
+
+    private static int requestsCount = 0;
+    private static String[] requests = new String[100];
+
+    private static void printRequests() {
+        for (int i = 0; i < requestsCount; ++i) {
+            System.out.printf("  %d. %s\n", i + 1, requests[i]);
+        }
+    }
+
     public static void main(String[] args) {
-        String logo = "" +
-            "                 _______\n"+
-            "               _/       \\_\n"+
-            "              / |       | \\\n"+
-            "             /  |__   __|  \\\n"+
-            "            |__/((o| |o))\\__|\n"+
-            "            |      | |      |\n"+
-            "            |\\     |_|     /|\n"+
-            "            | \\           / |\n"+
-            "             \\| /  ___  \\ |/\n"+
-            "              \\ | / _ \\ | /\n"+
-            "               \\_________/\n"+
-            "                _|_____|_\n"+
-            "           ____|_________|____\n"+
-            "          /                   \\\n";
+        String logo =
+                ""
+                        + "                 _______\n"
+                        + "               _/       \\_\n"
+                        + "              / |       | \\\n"
+                        + "             /  |__   __|  \\\n"
+                        + "            |__/((o| |o))\\__|\n"
+                        + "            |      | |      |\n"
+                        + "            |\\     |_|     /|\n"
+                        + "            | \\           / |\n"
+                        + "             \\| /  ___  \\ |/\n"
+                        + "              \\ | / _ \\ | /\n"
+                        + "               \\_________/\n"
+                        + "                _|_____|_\n"
+                        + "           ____|_________|____\n"
+                        + "          /                   \\\n";
         System.out.println(logo);
         System.out.println("Hello. What can I do for you?");
 
@@ -26,16 +37,21 @@ public class Duke {
         while (isRunning) {
             String cmd = in.nextLine();
 
-            System.out.print("  ");
             switch (cmd.toLowerCase()) {
+            case "list":
+                printRequests();
+                break;
             case "bye":
                 System.out.println("Bye. Hope to see you again soon!");
                 isRunning = false;
                 break;
             default:
-                System.out.println(cmd);
+                System.out.printf(" added: %s\n", cmd);
+                requests[requestsCount++] = cmd;
                 break;
             }
+
+            System.out.println("");
         }
 
         in.close();
