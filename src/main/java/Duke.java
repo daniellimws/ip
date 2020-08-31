8f1e59602875993fab68b7e5121c10b272369512
Daniel Lim Wee Soong <weesoong.lim@gmail.com>
2020-08-31 18:24:22 +0800

    Refactor constants and ensure SLAP

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,74 +7,101 @@ import java.util.Scanner;
 
 public class Duke {
 
+    private static final String REGEX_NUM = "\\d+";
+
+    private static final int TASKS_CAPACITY = 100;
+
+    private static final String AT_ARGUMENT = "/at ";
+    private static final String BY_ARGUMENT = "/by ";
+
+    private static final String BYE_COMMAND = "bye";
+    private static final String DONE_COMMAND = "done";
+    private static final String LIST_COMMAND = "list";
+
+    private static final String EVENT_COMMAND = "event ";
+    private static final String DEADLINE_COMMAND = "deadline ";
+    private static final String TODO_COMMAND = "todo ";
+
+    private static final String PROMPT = "> ";
+
     private static int taskCount = 0;
-    private static Task[] tasks = new Task[100];
+    private static Task[] tasks = new Task[TASKS_CAPACITY];
+
+    private static void printPrompt() {
+        System.out.print(PROMPT);
+    }
+
+    private static void printResponse(String response) {
+        System.out.printf("  %s\n", response);
+    }
 
     private static void printTasks() {
         if (taskCount == 0) {
-            System.out.println("There are currently no tasks in your list.");
+            printResponse("There are currently no tasks in your list.");
             return;
         }
-        System.out.println("Here are the tasks in your list:");
+        printResponse("Here are the tasks in your list:");
         for (int i = 0; i < taskCount; ++i) {
-            System.out.printf("  %d. %s\n", i + 1, tasks[i]);
+            printResponse(String.format("%d. %s", i + 1, tasks[i]));
         }
     }
 
     private static void markTaskDone(String cmd) {
         String[] words = cmd.split(" ");
-        if (words.length > 1 && words[1].matches("\\d+")) {
+        if (words.length > 1 && words[1].matches(REGEX_NUM)) {
             int index = Integer.parseInt(words[1]);
             if (index <= taskCount && index > 0) {
                 tasks[index - 1].markAsDone();
 
-                System.out.println("Ok! I've marked this task as done:");
-                System.out.printf("  %s\n", tasks[index - 1]);
-            } else System.out.printf("Error: Task %d does not exist\n", index);
+                printResponse("Ok! I've marked this task as done:");
+                printResponse(String.format("  %s", tasks[index - 1]));
+            } else System.out.printf("Error: Task %d does not exist", index);
         } else {
-            System.out.println("Usage: done [task index]");
-            System.out.println("Example: done 1");
+            printResponse("Usage: done [task index]");
+            printResponse("Example: done 1");
         }
     }
 
     private static Todo parseTodoCommand(String cmd) {
-        int descriptionIndex = "todo ".length();
+        int descriptionIndex = TODO_COMMAND.length();
         String description = cmd.substring(descriptionIndex);
         return new Todo(description);
     }
 
     private static Deadline parseDeadlineCommand(String cmd) {
-        int descriptionIndex = "deadline ".length();
-        int dateIndex = cmd.indexOf("/by ");
+        int descriptionIndex = DEADLINE_COMMAND.length();
+        int dateIndex = cmd.indexOf(BY_ARGUMENT);
         String description = cmd.substring(descriptionIndex, dateIndex - 1);
-        String date = cmd.substring(dateIndex + "/by ".length());
+        String date = cmd.substring(dateIndex + BY_ARGUMENT.length());
         return new Deadline(description, date);
     }
 
     private static Event parseEventCommand(String cmd) {
-        int descriptionIndex = "event ".length();
-        int dateIndex = cmd.indexOf("/at ");
+        int descriptionIndex = EVENT_COMMAND.length();
+        int dateIndex = cmd.indexOf(AT_ARGUMENT);
         String description = cmd.substring(descriptionIndex, dateIndex - 1);
-        String date = cmd.substring(dateIndex + "/at ".length());
+        String date = cmd.substring(dateIndex + AT_ARGUMENT.length());
         return new Event(description, date);
     }
 
     private static Task parseTaskCommand(String cmd) {
-        if (cmd.startsWith("todo ")) {
+        if (cmd.startsWith(TODO_COMMAND)) {
             return parseTodoCommand(cmd);
-        } else if (cmd.startsWith("deadline ")) {
+        } else if (cmd.startsWith(DEADLINE_COMMAND)) {
             return parseDeadlineCommand(cmd);
-        } else if (cmd.startsWith("event ")) {
+        } else if (cmd.startsWith(EVENT_COMMAND)) {
             return parseEventCommand(cmd);
         }
         return new Task(cmd);
     }
 
     private static void addTask(Task task) {
-        System.out.printf(" Added: %s\n", task);
+        printResponse(String.format("Added: %s", task));
         tasks[taskCount++] = task;
-        System.out.printf(
-                " You now have %d task%s in your list.", taskCount, (taskCount == 1 ? "" : "s"));
+        printResponse(
+                String.format(
+                        "You now have %d task%s in your list.",
+                        taskCount, (taskCount == 1 ? "" : "s")));
     }
 
     public static void main(String[] args) {
@@ -100,14 +127,15 @@ public class Duke {
         Scanner in = new Scanner(System.in);
 
         while (true) {
-            System.out.print("> ");
+            printPrompt();
             String cmd = in.nextLine();
 
-            if (cmd.equals("bye")) {
+            if (cmd.equals(BYE_COMMAND)) {
+                printResponse("Bye. See you next time.");
                 break;
-            } else if (cmd.equals("list")) {
+            } else if (cmd.equals(LIST_COMMAND)) {
                 printTasks();
-            } else if (cmd.startsWith("done")) {
+            } else if (cmd.startsWith(DONE_COMMAND)) {
                 markTaskDone(cmd);
             } else {
                 addTask(parseTaskCommand(cmd));
